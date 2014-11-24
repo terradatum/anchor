7f3137c73dded2631879d56e95527245dc2e542d
Julie Morahan <jmorahan@terradatum.com>
2014-11-24 13:40:33 -0700

    Beginning of postgres crt rewrites for Knots, Anchors, & Attributes.  Knots are done.  There is one section remaining in CreateAnchorsAndAttributes.js marked FIX ME that needs to be completed.

---
+++ SQL/PostgresSQL/crt/CreateAnchorsAndAttributes.js
@@ -0,0 +1,170 @@
+/*~
+-- ANCHORS AND ATTRIBUTES ---------------------------------------------------------------------------------------------
+--
+-- Anchors are used to store the identities of entities.
+-- Anchors are immutable.
+-- Attributes are used to store values for properties of entities.
+-- Attributes are mutable, their values may change over one or more types of time.
+-- Attributes have four flavors: static, historized, knotted static, and knotted historized.
+-- Anchors may have zero or more adjoined attributes.
+--
+~*/
+var anchor;
+while (anchor = schema.nextAnchor()) {
+    if(anchor.isGenerator())
+        switch (anchor.identity) {
+            case 'smallint': anchor.identityGenerator = 'smallserial'; break;
+            case 'bigint': anchor.identityGenerator = 'bigserial'; break;
+            default: anchor.identityGenerator = 'serial'; break;
+        }
+/*~
+-- Anchor table -------------------------------------------------------------------------------------------------------
+-- $anchor.name table (with ${(anchor.attributes ? anchor.attributes.length : 0)}$ attributes)
+-----------------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS \"$anchor.name\" (
+    $anchor.identityColumnName $anchor.identityGenerator not null,
+    $(schema.METADATA)? $anchor.metadataColumnName $schema.metadata.metadataType not null, : $anchor.dummyColumnName bit null,
+    constraint pk$anchor.name primary key (
+        $anchor.identityColumnName
+    )
+);
+~*/
+    var knot, attribute;
+    while (attribute = anchor.nextAttribute()) {
+        //if(attribute.isGenerator())
+        switch (attribute.identity) {
+            case 'smallint': attribute.identityGenerator = 'smallserial'; break;
+            case 'bigint': attribute.identityGenerator = 'bigserial'; break;
+            default: attribute.identityGenerator = 'serial'; break;
+        }
+        if(attribute.isHistorized() && !attribute.isKnotted()) {
+/*~
+-- Historized attribute posit table -----------------------------------------------------------------------------------
+-- $attribute.positName table (on $anchor.name)
+-----------------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS \"$attribute.positName\" (
+    $attribute.identityColumnName $attribute.identityGenerator not null,
+    $attribute.anchorReferenceName $anchor.identity not null,
+    $attribute.valueColumnName $attribute.dataRange not null,
+    $(attribute.hasChecksum())? $attribute.checksumColumnName as cast(${schema.metadata.encapsulation}$.MD5(cast($attribute.valueColumnName as varbinary(max))) as varbinary(16)) persisted,
+    $attribute.changingColumnName $attribute.timeRange not null,
+    constraint fk$attribute.positName foreign key (
+        $attribute.anchorReferenceName
+    ) references \"$anchor.name\"($anchor.identityColumnName),
+    constraint pk$attribute.positName primary key (
+        $attribute.identityColumnName
+    ),
+    constraint uq$attribute.positName unique (
+        $attribute.anchorReferenceName,
+        $attribute.changingColumnName,
+        $(attribute.hasChecksum())? $attribute.checksumColumnName : $attribute.valueColumnName 
+    )
+);
+~*/
+    }
+    else if(attribute.isHistorized() && attribute.isKnotted()) {
+        knot = attribute.knot;
+/*~
+-- Knotted historized attribute posit table ---------------------------------------------------------------------------
+-- $attribute.positName table (on $anchor.name)
+-----------------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS \"$attribute.positName\" (
+    $attribute.identityColumnName $attribute.identityGenerator not null,
+    $attribute.anchorReferenceName $anchor.identity not null,
+    $attribute.knotReferenceName $knot.identity not null,
+    $attribute.changingColumnName $attribute.timeRange not null,
+    constraint fk_A_$attribute.positName foreign key (
+        $attribute.anchorReferenceName
+    ) references \"$anchor.name\"($anchor.identityColumnName),
+    constraint fk_K_$attribute.positName foreign key (
+        $attribute.knotReferenceName
+    ) references \"$knot.name\"(\"$knot.identityColumnName\"),
+    constraint pk$attribute.positName primary key (
+        $attribute.identityColumnName
+    ),
+    constraint uq$attribute.positName unique (
+        $attribute.anchorReferenceName,
+        $attribute.changingColumnName,
+        $attribute.knotReferenceName
+    )
+);
+~*/
+    }
+    else if(attribute.isKnotted()) {
+        knot = attribute.knot;
+/*~
+-- Knotted static attribute posit table -------------------------------------------------------------------------------
+-- $attribute.positName table (on $anchor.name)
+-----------------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS \"$attribute.positName\" (
+    $attribute.identityColumnName $attribute.identityGenerator not null,
+    $attribute.anchorReferenceName $anchor.identity not null,
+    $attribute.knotReferenceName $knot.identity not null,
+    constraint fk_A_$attribute.positName foreign key (
+        $attribute.anchorReferenceName
+    ) references \"$anchor.name\"($anchor.identityColumnName),
+    constraint fk_K_$attribute.positName foreign key (
+        $attribute.knotReferenceName
+    ) references \"$knot.name\"(\"$knot.identityColumnName\"),
+    constraint pk$attribute.positName primary key (
+        $attribute.identityColumnName
+    ),
+    constraint uq$attribute.positName unique (
+        $attribute.anchorReferenceName,
+        $attribute.knotReferenceName
+    )
+);
+~*/
+    }
+    else {
+/*~
+-- Static attribute posit table -----------------------------------------------------------------------------------
+-- $attribute.positName table (on $anchor.name)
+-----------------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS \"$attribute.positName\" (
+    $attribute.identityColumnName $attribute.identityGenerator not null,
+    $attribute.anchorReferenceName $anchor.identity not null,
+    $attribute.valueColumnName $attribute.dataRange not null,
+    $(attribute.hasChecksum())? $attribute.checksumColumnName as cast(${schema.metadata.encapsulation}$.MD5(cast($attribute.valueColumnName as varbinary(max))) as varbinary(16)) persisted,
+    constraint fk$attribute.positName foreign key (
+        $attribute.anchorReferenceName
+    ) references \"$anchor.name\"($anchor.identityColumnName),
+    constraint pk$attribute.positName primary key (
+        $attribute.identityColumnName
+    ),
+    constraint uq$attribute.positName unique (
+        $attribute.anchorReferenceName,
+        $(attribute.hasChecksum())? $attribute.checksumColumnName : $attribute.valueColumnName
+    )
+);
+~*/
+    }
+    var scheme = schema.PARTITIONING ? ' ON PositorScheme(' + attribute.positorColumnName + ')' : '';
+/*~
+-- Attribute annex table ----------------------------------------------------------------------------------------------
+-- $attribute.annexName table (of $attribute.positName on $anchor.name)
+-----------------------------------------------------------------------------------------------------------------------
+CREATE TABLE IF NOT EXISTS \"$attribute.annexName\" (
+    $attribute.identityColumnName $attribute.identityGenerator not null,
+    $attribute.positingColumnName $schema.metadata.positingRange not null,
+    $attribute.positorColumnName $schema.metadata.positorRange not null,
+    $attribute.reliabilityColumnName $schema.metadata.reliabilityRange not null,
+    -- *** FIX ME *** --
+    --$attribute.reliableColumnName as isnull(cast(
+    --    case
+    --        when $attribute.reliabilityColumnName < $schema.metadata.reliableCutoff then 0
+    --        else 1
+    --    end
+    --as tinyint), 1) persisted,
+    $(schema.METADATA)? $attribute.metadataColumnName $schema.metadata.metadataType not null,
+    constraint fk$attribute.annexName foreign key (
+        $attribute.identityColumnName
+    ) references \"$attribute.positName\"($attribute.identityColumnName),
+    constraint pk$attribute.annexName primary key (
+        $attribute.identityColumnName,
+        $attribute.positorColumnName,
+        $attribute.positingColumnName
+    )
+)$scheme;
+~*/
+}}
