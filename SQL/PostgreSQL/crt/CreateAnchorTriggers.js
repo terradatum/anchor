3d88b906abe3a1efb303c71edb19ea90b620f9dc
Patrick Brannigan <pbrannigan@terradatum.com>
2014-12-12 16:27:08 -0800

    complete the insert trigger, started the update trigger - AERGO-546

--- SQL/PostgreSQL/crt/CreateAnchorTriggers.js
+++ SQL/PostgreSQL/crt/CreateAnchorTriggers.js
@@ -16,10 +16,10 @@ var anchor, knot, attribute;
 while (anchor = schema.nextAnchor()) {
     if(anchor.hasMoreAttributes()) {
 /*~
--- Insert trigger -----------------------------------------------------------------------------------------------------
--- it_l$anchor.name instead of INSERT trigger on l$anchor.name
+-- INSERT function -----------------------------------------------------------------------------------------------------
+-- if_l$anchor.name instead of INSERT trigger function on l$anchor.name
 -----------------------------------------------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION $anchor.capsule$.\"tf_l$anchor.name\"()
+CREATE OR REPLACE FUNCTION $anchor.capsule$.\"if_l$anchor.name\"()
     RETURNS trigger AS 
     $$BODY$$
     DECLARE \"now\" $schema.metadata.chronon;
@@ -77,16 +77,173 @@ CREATE OR REPLACE FUNCTION $anchor.capsule$.\"tf_l$anchor.name\"()
             }
         }
 /*~
-    );
+    ) ON COMMIT DROP;
+    INSERT INTO inserted
+    SELECT
+        COALESCE(i.$anchor.identityColumnName, a.$anchor.identityColumnName),
+        $(schema.METADATA)? i.$anchor.metadataColumnName,
+ ~*/
+        while (attribute = anchor.nextAttribute()) {
+/*~
+        $(schema.IMPROVED)? COALESCE(i.$attribute.anchorReferenceName, i.$anchor.identityColumnName, a.$anchor.identityColumnName),
+        $(schema.METADATA)? COALESCE(i.$attribute.metadataColumnName, i.$anchor.metadataColumnName),
+        $(attribute.isHistorized())? COALESCE(i.$attribute.changingColumnName, now),
+        COALESCE(i.$attribute.positorColumnName, i.$schema.metadata.positorSuffix, 0),
+        COALESCE(i.$attribute.positingColumnName, now),
+        COALESCE(i.$attribute.reliabilityColumnName,
+        CASE
+            WHEN i.$schema.metadata.reliableSuffix = 0 THEN $schema.metadata.deleteReliability
+            WHEN i.$attribute.reliableColumnName = 0 THEN $schema.metadata.deleteReliability
+            ELSE $schema.metadata.reliableCutoff
+        END),
+~*/
+            if(attribute.isKnotted()) {
+                knot = attribute.knot;
+/*~
+        i.$attribute.knotValueColumnName,
+        $(knot.hasChecksum())? COALESCE(i.$attribute.knotChecksumColumnName, ${schema.metadata.encapsulation}$.MD5(cast(i.$attribute.knotValueColumnName as varbinary(max)))),
+        $(schema.METADATA)? COALESCE(i.$attribute.knotMetadataColumnName, i.$anchor.metadataColumnName),
+~*/
+            }
+/*~
+        i.$attribute.valueColumnName$(anchor.hasMoreAttributes())?,
+~*/
+        }
+/*~
+    FROM (
+        SELECT
+            $schema.metadata.positorSuffix,
+            $schema.metadata.reliableSuffix,
+            $anchor.identityColumnName,
+            $(schema.METADATA)? $anchor.metadataColumnName,
+ ~*/
+        while (attribute = anchor.nextAttribute()) {
+/*~
+            $(schema.IMPROVED)? $attribute.anchorReferenceName,
+            $(schema.METADATA)? $attribute.metadataColumnName,
+            $(attribute.isHistorized())? $attribute.changingColumnName,
+            $attribute.positorColumnName,
+            $attribute.positingColumnName,
+            $attribute.reliabilityColumnName,
+            $attribute.reliableColumnName,
+~*/
+            if(attribute.isKnotted()) {
+                knot = attribute.knot;
+/*~
+            $attribute.knotValueColumnName,
+            $(knot.hasChecksum())? $attribute.knotChecksumColumnName,
+            $(schema.METADATA)? $attribute.knotMetadataColumnName,
+~*/
+            }
+/*~
+            $attribute.valueColumnName,
+~*/
+        }
+/*~
+            ROW_NUMBER() OVER () AS Row
+        FROM
+            inserted
+    ) i
+    LEFT JOIN
+        $anchor.mnemonic a
+    ON
+        a.Row = i.Row;
+~*/
+        while (attribute = anchor.nextAttribute()) {
+            knot = attribute.knot;
+/*~
+    INSERT INTO $attribute.capsule$.\"$attribute.name\" (
+        $(schema.METADATA)? $attribute.metadataColumnName,
+        $attribute.anchorReferenceName,
+        $attribute.valueColumnName,
+        $(attribute.timeRange)? $attribute.changingColumnName,
+        $attribute.positingColumnName,
+        $attribute.positorColumnName,
+        $attribute.reliabilityColumnName
+    )
+    SELECT
+        $(schema.METADATA)? i.$attribute.metadataColumnName,
+        i.$attribute.anchorReferenceName,
+        $(attribute.isKnotted())? COALESCE(i.$attribute.valueColumnName, k$knot.mnemonic$.$knot.identityColumnName), : i.$attribute.valueColumnName,
+        $(attribute.timeRange)? i.$attribute.changingColumnName,
+        i.$attribute.positingColumnName,
+        i.$attribute.positorColumnName,
+        i.$attribute.reliabilityColumnName
+    FROM
+        inserted i
+~*/
+            if(attribute.isKnotted()) {
+/*~
+    LEFT JOIN
+        $knot.capsule$.\"$knot.name\" k$knot.mnemonic
+    ON
+        $(knot.hasChecksum())? k$knot.mnemonic$.$knot.checksumColumnName = i.$attribute.knotChecksumColumnName : k$knot.mnemonic$.$knot.valueColumnName = i.$attribute.knotValueColumnName
+    WHERE
+        COALESCE(i.$attribute.valueColumnName, k$knot.mnemonic$.$knot.identityColumnName) IS NOT NULL;
+~*/
+            }
+            else {
+/*~
+    WHERE
+        i.$attribute.valueColumnName IS NOT NULL;
+~*/
+            }
+        }
+/*~
+
     RETURN inserted;
     END;
     $$BODY$$
     LANGUAGE plpgsql;
 
+-- Insert trigger -----------------------------------------------------------------------------------------------------
+-- it_l$anchor.name instead of INSERT trigger on l$anchor.name
+-----------------------------------------------------------------------------------------------------------------------
 DROP TRIGGER IF EXISTS \"it_l$anchor.name\" ON $anchor.capsule$.\"l$anchor.name\";
 CREATE TRIGGER \"it_l$anchor.name\" INSTEAD OF INSERT ON $anchor.capsule$.\"l$anchor.name\"
     FOR EACH ROW
-    EXECUTE PROCEDURE $anchor.capsule$.\"tf_l$anchor.name\"();
+    EXECUTE PROCEDURE $anchor.capsule$.\"if_l$anchor.name\"();
 ~*/
-            }
+    }
+    if(anchor.hasMoreAttributes()) {
+/*~
+-- UPDATE function -----------------------------------------------------------------------------------------------------
+-- uf_l$anchor.name instead of UPDATE trigger function on l$anchor.name
+-----------------------------------------------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION $anchor.capsule$.\"uf_l$anchor.name\"()
+    RETURNS trigger AS 
+    $$BODY$$
+    DECLARE \"now\" $schema.metadata.chronon;
+    \"id\" $anchor.identity;
+    BEGIN
+    now := $schema.metadata.now;
+   IF(OLD.$anchor.identityColumnName != NEW.$anchor.identityColumnName)
+        THEN RAISE EXCEPTION 'The identity column $anchor.identityColumnName is not updatable.';
+    END IF;
+~*/
+        while (attribute = anchor.nextAttribute()) {
+/*~
+    IF(OLD.$attribute.identityColumnName != NEW.$attribute.identityColumnName)
+        THEN RAISE EXCEPTION 'The identity column $attribute.identityColumnName is not updatable.';
+    END IF;
+    IF(OLD.$attribute.anchorReferenceName != NEW.$attribute.anchorReferenceName)
+        THEN RAISE EXCEPTION 'The foreign key column $attribute.anchorReferenceName is not updatable.';
+    END IF;
+~*/
         }
+/*~
+    RETURN null;
+    END;
+    $$BODY$$
+    LANGUAGE plpgsql;
+
+-- UPDATE trigger -----------------------------------------------------------------------------------------------------
+-- ut_l$anchor.name instead of UPDATE trigger on l$anchor.name
+-----------------------------------------------------------------------------------------------------------------------
+DROP TRIGGER IF EXISTS \"ut_l$anchor.name\" ON $anchor.capsule$.\"l$anchor.name\";
+CREATE TRIGGER \"ut_l$anchor.name\" INSTEAD OF UPDATE ON $anchor.capsule$.\"l$anchor.name\"
+    FOR EACH ROW
+    EXECUTE PROCEDURE $anchor.capsule$.\"uf_l$anchor.name\"();
+~*/
+    }
+}
